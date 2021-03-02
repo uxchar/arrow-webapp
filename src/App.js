f6ad34f1083c94764b09926b1eabfdd5e808f46b
uxchar <c33.j@outlook.com>
2021-03-01 23:03:19 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Navbar from "./Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
     </div>
   );
-}
+  //   function greet() {
+  //     let greeting = document.querySelector(".greeting");
 
+  //     const date = new Date();
+  //     const hours = date.getHours();
+  //     let timeOfDay;
+
+  //     if (hours < 12) {
+  //       timeOfDay = "morning";
+  //     } else if (hours >= 12 && hours < 17) {
+  //       timeOfDay = "afternoon";
+  //     } else {
+  //       timeOfDay = "evening";
+  //     }
+
+  //     return (greeting.textContent = `Good ${timeOfDay}. Would you like to add a task?`);
+  //   }
+
+  //   function addTask() {
+  //     let user_input = document.querySelector(".user_input");
+
+  //     if (user_input == "yes" || "y") {
+  //       greeting.textContent = "What would you like to add?";
+  //     } else if (user_input == "no" || "n") {
+  //       greeting.textContent = "Goodbye";
+  //     } else {
+  //       greeting.textContent = "I did not understand, please type yes or no.";
+  //     }
+  //   }
+  //   greet();
+  //   addTask();
+  // }
+  // App();
+}
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <header class="nav">
+        <a className="site-logo" href="/">
+          Arrow
+        </a>
+        <div className="hamburger">
+          <div className="wrapper-menu">
+            <div className="line-menu half start"></div>
+            <div className="line-menu"></div>
+            <div className="line-menu half end"></div>
+          </div>
+        </div>
+
+        <ul className="nav-links">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </header>
+      <h1 className="greeting fade-in"></h1>\{" "}
+    </div>
+  );
+}
+
+export default Navbar;
