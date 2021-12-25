e459c1613c3bb61a70dd1bbf8f84f63ba28359ca
uxchar <c33.j@outlook.com>
2021-12-25 09:51:28 -0500

    updated Inspiration component

--- src/components/Inspiration.js
+++ src/components/Inspiration.js
@@ -9,19 +9,6 @@ function Inspiration() {
   }, []);
 
   function fetchQuote() {
-    var express = require("express");
-    var cors = require("cors");
-    var app = express();
-
-    app.use(cors());
-
-    app.get("/products/:id", function (req, res, next) {
-      res.json({ msg: "This is CORS-enabled for all origins!" });
-    });
-
-    app.listen(80, function () {
-      console.log("CORS-enabled web server listening on port 80");
-    });
     fetch("http://quotes.rest/qod.json")
       .then((res) => res.json())
       .then((data) => {
