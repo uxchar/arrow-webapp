088fb3321f678423f188de0e25af3011cdc8bcdf
uxchar <c33.j@outlook.com>
2021-12-25 09:46:49 -0500

    updated Inspiration component

--- src/components/Inspiration.js
+++ src/components/Inspiration.js
@@ -1,19 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-var express = require("express");
-var cors = require("cors");
-var app = express();
-
-app.use(cors());
-
-app.get("/products/:id", function (req, res, next) {
-  res.json({ msg: "This is CORS-enabled for all origins!" });
-});
-
-app.listen(80, function () {
-  console.log("CORS-enabled web server listening on port 80");
-});
-
 function Inspiration() {
   const [author, setAuthor] = useState("");
   const [quote, setQuote] = useState("");
@@ -23,6 +9,19 @@ function Inspiration() {
   }, []);
 
   function fetchQuote() {
+    var express = require("express");
+    var cors = require("cors");
+    var app = express();
+
+    app.use(cors());
+
+    app.get("/products/:id", function (req, res, next) {
+      res.json({ msg: "This is CORS-enabled for all origins!" });
+    });
+
+    app.listen(80, function () {
+      console.log("CORS-enabled web server listening on port 80");
+    });
     fetch("http://quotes.rest/qod.json")
       .then((res) => res.json())
       .then((data) => {
